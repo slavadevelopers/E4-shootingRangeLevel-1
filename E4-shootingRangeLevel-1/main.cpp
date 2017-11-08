a06531382c12c28069d19216e430d1cf6860ab92
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-11-08 22:53:29 +0300

    Test E4 is completed

---
+++ E4-shootingRangeLevel-1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x <= 1 && x >= -4) {
+        cout << 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
+    return 0;
+}
